94f6683d47cbfd3c1d9ebbd44b106cea136c25a1
Tetiana Blizniukova <tatbliz30@gmail.com>
2021-08-16 01:23:58 +0300

    done HW

---
+++ index.js
@@ -0,0 +1,51 @@
+const refs = {
+    timer: document.querySelector('#timer'),
+    days: document.querySelector('[data-value="days"]'),
+    hours: document.querySelector('[data-value="hours"]'),
+    minutes: document.querySelector('[data-value="mins"]'),
+    seconds: document.querySelector('[data-value="secs"]'),
+};
+
+class CountdownTimer {
+    constructor({ selector, targetDate }) {
+        this.selector = selector;
+        this.targetDate = targetDate;
+    }
+
+    interval = setInterval(() => {
+        const currentTime = Date.now();
+        const delta = this.targetDate - currentTime;
+        this.getTimeComponents(delta);
+        this.updateTimer(delta);
+    }, 1000);
+    pad(value) {
+        return String(value).padStart(2, '0');
+    }
+
+    getTimeComponents(time) {
+        const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+        const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+        const minutes = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+        const seconds = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+        refs.days.textContent = `${days}`;
+        refs.hours.textContent = `${hours}`;
+        refs.minutes.textContent = `${minutes}`;
+        refs.seconds.textContent = `${seconds}`;
+    }
+
+    stopTimer(time) {
+        if (time < 0) {
+            refs.timer.textContent = 'The End!';
+            refs.timer.style.textAlign = `center`;
+            refs.timer.style.fontSize = `250px`;
+            refs.timer.style.marginTop = `50px`;
+            refs.timer.style.color = `yellow`;
+
+        }
+    }
+};
+
+const newTimer = new CountdownTimer({
+  selector: '#timer-1',
+  targetDate: new Date('Aug 27, 2021'),
+});
